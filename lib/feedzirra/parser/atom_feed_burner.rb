cd77166e61edaf8f07ab3b11638eb3dc1776b236
Kalvir Sandhu <kalv@kalv.co.uk>
2009-10-08 16:29:49 +0100

    added push links extraction to atom feed burner

--- lib/feedzirra/parser/atom_feed_burner.rb
+++ lib/feedzirra/parser/atom_feed_burner.rb
@@ -16,9 +16,17 @@ module Feedzirra
       element :link, :as => :url, :value => :href, :with => {:type => "text/html"}
       element :link, :as => :feed_url, :value => :href, :with => {:type => "application/atom+xml"}
       elements :entry, :as => :entries, :class => AtomFeedBurnerEntry
+      
+      elements :link, :as=>:hub_push_links, :value=>:href, :with=>{:rel=>"hub"}
+      elements :"atom:link", :as => :atom_push_links, :value=>:href, :with=>{:rel=>"hub"}
+      elements :"atom10:link", :as => :atom10_push_links, :value=>:href, :with=>{:rel=>"hub"}
 
       def self.able_to_parse?(xml) #:nodoc:
         (xml =~ /Atom/ && xml =~ /feedburner/) || false
+      end
+      
+      def push_links
+        @hub_push_links || @atom_push_links || @atom10_push_links
       end
     end
 
